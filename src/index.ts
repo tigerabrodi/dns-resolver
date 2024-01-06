5563fb8e90d38e6dfc36743f9f9115a34276c572
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-06 11:56:22 +0100

    fix server: udp4 for ipv4address and udp6 for ipv6adress

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dgram from 'node:dgram'
+import net from 'node:net'
 
 import { createDNSQuery } from './createDNSQuery'
 import { parseResponse } from './parseResponse'
@@ -10,91 +11,76 @@ export interface DNSRecord {
   offset: number
 }
 
-const client = dgram.createSocket('udp4')
 const ROOT_DNS_SERVER = '198.41.0.4'
 const DNS_PORT = 53
 export const A_RECORD_TYPE = 1
 export const AAAA_RECORD_TYPE = 28
 export const NS_RECORD_TYPE = 2
+const DOMAIN_TO_RESOLVE = 'www.google.com'
 
-function queryDNS(domain: string, server: string) {
+function queryDNS(domain: string, server: string, isInitialQuery = false) {
+  console.log(`Querying ${server} for ${domain}`)
+  console.log('-----------------------------------', isInitialQuery)
+
+  // Determine if the server address is IPv4 or IPv6
+  const ipVersion = net.isIP(server)
+  console.log('ipVersion', ipVersion)
+  const socketType = ipVersion === 6 ? 'udp6' : 'udp4'
+
+  // Create a new socket for this query
+  const client = dgram.createSocket(socketType)
+
   const dnsQuery = createDNSQuery(domain)
-  client.send(dnsQuery, DNS_PORT, server, handleError)
-}
 
-client.on('message', (message) => {
-  handleDNSResponse(message)
-})
+  client.send(dnsQuery, DNS_PORT, server, (error) => {
+    if (error) {
+      console.error('Error sending DNS query:', error)
+      client.close()
+    } else {
+      console.log(`DNS Query sent to ${server}:${DNS_PORT}`)
+    }
+  })
 
-function handleDNSResponse(message: Buffer) {
-  const response = parseResponse(message)
-  processDNSResponse(response)
+  client.on('message', (message) => {
+    const response = parseResponse(message)
+    processDNSResponse(response)
+    client.close() // Close the socket after processing the response
+  })
+
+  client.on('error', (err) => {
+    console.error(`Client error: ${err.message}`)
+    client.close()
+  })
+
+  client.on('timeout', () => {
+    console.log('Request timed out')
+    client.close()
+  })
 }
 
 function processDNSResponse(response: ReturnType<typeof parseResponse>) {
-  const nsRecords = findNSRecords(response.authorityRecords)
+  response.authorityRecords
+    .filter((rec) => rec.type === NS_RECORD_TYPE)
+    .forEach((nsRecord) => {
+      const nsIP = response.additionalRecords.find(
+        (rec) => rec.domainName === nsRecord.rdata
+      )?.rdata
 
-  if (nsRecords.length > 0) {
-    queryNameServers(nsRecords, response.additionalRecords)
-  } else {
-    logFinalIPAddresses(response.answerRecords)
-  }
-}
+      console.log('nsIP', nsIP)
 
-function findNSRecords(authorityRecords: Array<DNSRecord>) {
-  return authorityRecords.filter((rec) => rec.type === NS_RECORD_TYPE)
-}
+      if (nsIP) {
+        queryDNS(DOMAIN_TO_RESOLVE, nsIP) // Replace with the domain you are resolving
+      }
+    })
 
-function queryNameServers(
-  nsRecords: Array<DNSRecord>,
-  additionalRecords: Array<DNSRecord>
-) {
-  nsRecords.forEach((nsRecord) => {
-    const nsIP = findNSIPAddress(nsRecord.rdata, additionalRecords)
-    if (nsIP) {
-      queryDNS('www.google.com', nsIP) // Replace with the domain you are resolving
-    }
-  })
-}
-
-function findNSIPAddress(
-  nsDomainName: string,
-  additionalRecords: Array<DNSRecord>
-) {
-  const record = additionalRecords.find(
-    (rec) => rec.domainName === nsDomainName
-  )
-
-  return record ? record.rdata : null
-}
-
-function logFinalIPAddresses(answerRecords: Array<DNSRecord>) {
-  answerRecords.forEach((record) => {
-    if (record.type === A_RECORD_TYPE || record.type === AAAA_RECORD_TYPE) {
-      console.log(`IP Address found: ${record.rdata}`)
-    }
-  })
-  client.close()
-}
-
-function handleError(error: Error | null) {
-  if (error) {
-    console.error('Error:', error)
-    client.close()
-  } else {
-    console.log(`DNS Query sent to ${ROOT_DNS_SERVER}:${DNS_PORT}`)
+  if (response.authorityRecords.length === 0) {
+    response.answerRecords
+      .filter(
+        (rec) => rec.type === A_RECORD_TYPE || rec.type === AAAA_RECORD_TYPE
+      )
+      .forEach((record) => console.log(`IP Address found: ${record.rdata}`))
   }
 }
 
 // Initial Query
-queryDNS('www.google.com', ROOT_DNS_SERVER)
-
-client.on('error', (err) => {
-  console.log(`Client error: ${err.message}`)
-  client.close()
-})
-
-client.on('timeout', () => {
-  console.log('Request timed out')
-  client.close()
-})
+queryDNS(DOMAIN_TO_RESOLVE, ROOT_DNS_SERVER, true)
